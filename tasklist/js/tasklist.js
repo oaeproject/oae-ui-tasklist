6d004bc187ea636b120bb3de735f299eefd7bf86
Bert Pareyn <pareyn.bert@gmail.com>
2014-05-29 16:28:15 +0100

    Feature: Add and list tasks

--- tasklist/js/tasklist.js
+++ tasklist/js/tasklist.js
@@ -21,12 +21,91 @@ define(['jquery', 'oae.core'], function($, oae) {
         var $rootel = $('#' + uid);
 
         /**
-         * Initialize the tasklist widget
+         * Create a new task
          */
-        var initTaskList = function() {
+        var createTask = function() {
+            $.ajax({
+                'url': '/api/tasklist',
+                'type': 'POST',
+                'data': $('#tasklist-task-form', $rootel).serialize(),
+                'success': function(data) {
+                    oae.api.util.notification(
+                        oae.api.i18n.translate('__MSG__TASK_CREATED__', 'tasklist'),
+                        oae.api.i18n.translate('__MSG__TASK_CREATE_SUCCESS__', 'tasklist')
+                    );
+                    getTasks();
+                    $('#tasklist-task-form', $rootel)[0].reset();
+                },
+                'error': function() {
+                    oae.api.util.notification(
+                        oae.api.i18n.translate('__MSG__TASK_NOT_CREATED__', 'tasklist'),
+                        oae.api.i18n.translate('__MSG__TASK_CREATE_FAILED__', 'tasklist'),
+                        'error'
+                    );
+                }
+            });
 
+            return false;
         };
 
+        /**
+         * Render the list of tasks
+         *
+         * @param  {Task[]}    tasks    Array of task objects
+         */
+        var renderTaskList = function(tasks) {
+            oae.api.util.template().render($('#tasklist-tasks-template', $rootel), {
+                'tasks': tasks
+            }, $('#tasklist-tasks-container', $rootel));
+        };
+
+        /**
+         * Retrieve the list of tasks
+         */
+        var getTasks = function() {
+            $.ajax({
+                'url': '/api/tasklist',
+                'type': 'GET',
+                'success': function(data) {
+                    // Render the list of tasks in the UI
+                    renderTaskList(data.tasks);
+                },
+                'error': function() {
+                    oae.api.util.notification(
+                        oae.api.i18n.translate('__MSG__TASKS_NOT_RETRIEVED__', 'tasklist'),
+                        oae.api.i18n.translate('__MSG__TASKS_RETRIEVE_FAILED__', 'tasklist'),
+                        'error'
+                    );
+                }
+            });
+        };
+
+        /**
+         * Bind to various elements in the widget
+         */
+        var addBinding = function() {
+            $rootel.on('submit', '#tasklist-task-form', createTask);
+        };
+
+        /**
+         * Render the tasklist header
+         */
+        var renderTaskListHeader = function() {
+            oae.api.util.template().render($('#tasklist-header-template', $rootel), null, $('#tasklist-header-container', $rootel));
+        };
+
+        /**
+         * Initialize the task list widget
+         */
+        var initTaskList = function() {
+            // Render the task list header
+            renderTaskListHeader();
+
+            // Get the tasks
+            getTasks();
+        };
+
+        addBinding();
         initTaskList();
 
     };
